3e71b8aca1e61dc765fa34d22be57ecc0b4ce46b
sanjarbek <sanjarsaidov173@gamil.com>
2022-08-20 12:20:02 +0500

    homework

---
+++ telegram/message.py
@@ -0,0 +1,20 @@
+
+class Message:
+    def __init__(self, message) -> None:
+        pass
+
+    def fromDict(self)->dict:
+        '''
+        Convert User object to dictionary
+        Returns:
+            dict: dictionary of user data
+        '''
+        pass 
+
+    #Override the __str__ method to print the user data
+    def __str__(self):
+        '''
+        Print the user data
+        '''
+        pass
+    
