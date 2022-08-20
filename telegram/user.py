e368062f6679eef6eae979ba65533c13dfb46a93
ZARIFMAC <uzbzarif@gmail.com>
2022-08-20 11:42:45 +0500

    fromDict

--- telegram/user.py
+++ telegram/user.py
@@ -3,3 +3,21 @@ class User:
         self.id = user['id']
         self.first_name=user['first_name']
         self.username = user['username']
+
+
+    def fromDict(self)->dict:
+        '''
+        Convert User object to dictionary
+        Returns:
+            dict: dictionary of user data
+        '''
+        pass 
+
+    #Override the __str__ method to print the user data
+    def __str__(self):
+        '''
+        Print the user data
+        '''
+        pass
+    
+        
