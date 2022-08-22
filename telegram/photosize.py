dcb5dd33c81262ba622ef99125a66b5512d4ed2e
djumanov <diyorbekjumanov1120@gmail.com>
2022-08-22 22:16:55 +0500

    photo size from dict and print

--- telegram/photosize.py
+++ telegram/photosize.py
@@ -1,9 +1,11 @@
+import json
+
 class PhotoSize:
     def __init__(self, photo: dict) -> None:
         self.file_id = photo['file_id']
         self.file_unique_id = photo['file_unique_id']
-        self.width = photo['width']
-        self.height = photo['height']
+        self.width = photo.get('width')
+        self.height = photo.get('height')
         self.file_size = photo.get('file_size')
 
     def fromDict(self) -> dict:
@@ -11,11 +13,17 @@ class PhotoSize:
 
         photo_dict = {
             'file_id': self.file_id,
-            'file_unique_id': self.file_unique_id,
-            'width': self.width,
-            'height': self.height, 
+            'file_unique_id': self.file_unique_id, 
         }
         if self.file_size:
             photo_dict['file_size'] = self.file_size
+        if self.width:
+            photo_dict['width'] = self.width
+        if self.height:
+            photo_dict['height'] = self.height
         
         return photo_dict
+
+    
+    def __repr__(self) -> str:
+        return json.dumps(self.fromDict())
