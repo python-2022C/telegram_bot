e3f9488a8dede62866ad03f27b205e20d9bcbfca
djumanov <diyorbekjumanov1120@gmail.com>
2022-08-22 21:47:07 +0500

    photo size from dict

--- telegram/photosize.py
+++ telegram/photosize.py
@@ -6,4 +6,16 @@ class PhotoSize:
         self.height = photo['height']
         self.file_size = photo.get('file_size')
 
-    
+    def fromDict(self) -> dict:
+        '''This object represents one size of a photo or a file/sticker thumbnail.'''
+
+        photo_dict = {
+            'file_id': self.file_id,
+            'file_unique_id': self.file_unique_id,
+            'width': self.width,
+            'height': self.height, 
+        }
+        if self.file_size:
+            photo_dict['file_size'] = self.file_size
+        
+        return photo_dict
