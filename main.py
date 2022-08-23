9d98c57a5172c5d228427d8c17f1333f2e28aa09
djumanov <diyorbekjumanov1120@gmail.com>
2022-08-23 09:22:35 +0500

    test photosize

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ bot = telegram.Bot(TOKEN)
 updates=bot.getUpdates()
 last_update = updates[-1]
 chat_id = last_update.message.from_user.id
-text = last_update.message.text
-bot.sendMessage(chat_id, text)
+p = last_update.message.photo[0].file_id
+bot.sendPhoto(chat_id, p)
